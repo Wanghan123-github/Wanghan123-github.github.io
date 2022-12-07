c21d6aec05da9fa0b2101179dfd7f52aa49ab725
hwang <2484760160@qq.com>
2022-12-07 17:38:34 +0800

    Site updated: 2022-12-07 17:38:33

--- js/search/algolia.js
+++ js/search/algolia.js
@@ -82,7 +82,6 @@ window.addEventListener('load', () => {
     container: '#algolia-search-input',
     showReset: false,
     showSubmit: false,
-    searchOnEnterKeyPressOnly: true,
     placeholder: GLOBAL_CONFIG.algolia.languages.input_placeholder,
     showLoadingIndicator: true
   })
